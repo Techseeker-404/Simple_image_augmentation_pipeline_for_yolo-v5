eadf3d1e780ea90ac5631d7964746b15eb36a612
Techseeker-404 <anandml4u@gmail.com>
2021-09-29 14:44:28 +0530

    Added args provision for no.of augmentations.

--- augmentPipe.py
+++ augmentPipe.py
@@ -128,9 +128,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Plz pass arguments")
     parser.add_argument("-ipt", "--images_path", help="Path to images directory")
     parser.add_argument("-lpt", "--labels_path", help="Path to labels directory")
+    parser.add_argument("-Aug", "--num_of_Aug", help="No.of Augmentations per image", default=5)
 
     args = parser.parse_args()
 
     AugData = AugmentYoloData(args.images_path, args.labels_path)
-    AugData.augmenter(num_aug=3)
+    AugData.augmenter(num_aug=args.num_of_Aug)
 
